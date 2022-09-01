14e341d37bc88ea345ebf4c0aa8ea532ab1e80f7
Madhavi A <amadhavi@pkrmsezdc.com>
2022-09-01 21:58:34 +0530

    Finished Book

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
 import { MatSnackBar, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 import { Inject } from '@nestjs/common';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'tmo-reading-list',
@@ -13,6 +14,11 @@ export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
   actionButtonLabel: string = 'Undo';
   snackmessage: string = 'Your book has been removed from the reading list'
+  today = new Date();
+  changedDate = '';
+  pipe = new DatePipe('en-US');
+  isShowDivIf: boolean = false;
+
   // @Inject(MAT_SNACK_BAR_DATA) public data: any
   constructor(private readonly store: Store, private _snackBar: MatSnackBar) { }
 
@@ -22,4 +28,11 @@ export class ReadingListComponent {
       duration: 5000
     });
   }
+  formatDate() {
+    let ChangedFormat = this.pipe.transform(this.today, 'dd/MM/YYYY');
+    this.changedDate = ChangedFormat;
+    this.isShowDivIf = !this.isShowDivIf;
+    console.log(ChangedFormat, 'ChangedFormat')
+  }
+
 }
